498ef5d391a55ba39547b1af81e1df8d59008c58
Raghu Dungala <draghu222@gmail.com>
2023-07-09 13:02:25 -0500

    Updates

---
+++ precache-manifest.817d465e399d0c56a767a9119cdd42d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef1e30b6a4c6c311b44d",
+    "url": "/portfolio/static/css/main.d48dfcb9.chunk.css"
+  },
+  {
+    "revision": "ef1e30b6a4c6c311b44d",
+    "url": "/portfolio/static/js/main.ef1e30b6.chunk.js"
+  },
+  {
+    "revision": "e5846861b89810b99e9d",
+    "url": "/portfolio/static/js/1.e5846861.chunk.js"
+  },
+  {
+    "revision": "d72b43bfa23e050e18a1",
+    "url": "/portfolio/static/js/2.d72b43bf.chunk.js"
+  },
+  {
+    "revision": "b8716916fda4fec7c2ac",
+    "url": "/portfolio/static/js/runtime~main.b8716916.js"
+  },
+  {
+    "revision": "38786793f516d38d0052cd20849534c2",
+    "url": "/portfolio/static/media/python_original_logo_icon_146381.38786793.ico"
+  },
+  {
+    "revision": "042d5fa5f1aefaacfa304b6ab59327e7",
+    "url": "/portfolio/static/media/tensorflow_logo_icon_168671.042d5fa5.ico"
+  },
+  {
+    "revision": "45a61ce3058e44d42c7522ac16fa6f32",
+    "url": "/portfolio/static/media/award.45a61ce3.png"
+  },
+  {
+    "revision": "67c5a4cf4a226ada1aee7be567f7fc92",
+    "url": "/portfolio/static/media/star.67c5a4cf.png"
+  },
+  {
+    "revision": "2abacb77b3f1e391cce19185798f8f3c",
+    "url": "/portfolio/static/media/kclogo.2abacb77.jpg"
+  },
+  {
+    "revision": "0c5c04919d51f02f80119f45b105a9e1",
+    "url": "/portfolio/static/media/raghu.0c5c0491.JPEG"
+  },
+  {
+    "revision": "7b6a0abbabe5f665fc7e08e5aa808d13",
+    "url": "/portfolio/static/media/background.7b6a0abb.jpg"
+  },
+  {
+    "revision": "d930473c7415ac2065db5086d1e03221",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.987e094be8671901ea87bf47a0831d22.js"
+  "/portfolio/precache-manifest.817d465e399d0c56a767a9119cdd42d8.js"
 );
 
 workbox.clientsClaim();
