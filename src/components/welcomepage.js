0c8e39f23bbe3685d0158309cdd60c9dcaaa0c36
Raghu Dungala <draghu222@gmail.com>
2023-08-03 23:49:57 -0500

    CSS changes

--- src/components/welcomepage.js
+++ src/components/welcomepage.js
@@ -7,49 +7,12 @@ import personalImage from "../images/raghu.JPEG"
 const WelcomePage = () => {
   return (
     <section id="welcomepage">
-      {/* <div className="container1">
-        <div className="box">
-          <img src={award} alt="award" style={{ width: "100%" }}></img>
-        </div>
-        <div className="box">
-          <img src={star} alt="award" style={{ width: "100%" }}></img>
-        </div>
-        <div className="box">
-          <img src={award} alt="award" style={{ width: "100%" }}></img>
-        </div>
-        <div className="box">
-          <img src={star} alt="award" style={{ width: "100%" }}></img>
-        </div>
-      </div> */}
       <div className="circular-image">
         <img src={personalImage} alt="Personal" className="user-pic"></img>
-        
       </div>
       <h1 id="intro">
       <span role="img" aria-label="hi">👋</span>
-        <span className="dropping-letter">H</span>
-        <span className="dropping-letter">e</span>
-        <span className="dropping-letter">l</span>
-        <span className="dropping-letter">l</span>
-        <span className="dropping-letter">o</span>
-        <span className="dropping-letter">&nbsp;</span>
-        <span className="dropping-letter">I</span>
-        <span className="dropping-letter">'</span>
-        <span className="dropping-letter">m</span>
-        <span className="dropping-letter">&nbsp;</span>
-        <span className="dropping-letter">R</span>
-        <span className="dropping-letter">a</span>
-        <span className="dropping-letter">g</span>
-        <span className="dropping-letter">h</span>
-        <span className="dropping-letter">u</span>
-        <span className="dropping-letter">&nbsp;</span>
-        <span className="dropping-letter">D</span>
-        <span className="dropping-letter">u</span>
-        <span className="dropping-letter">n</span>
-        <span className="dropping-letter">g</span>
-        <span className="dropping-letter">a</span>
-        <span className="dropping-letter">l</span>
-        <span className="dropping-letter">a</span>
+      Hello I'm Raghu Dungala
       </h1>
       <h2>Coding with passion, building with excellence.</h2>
 
