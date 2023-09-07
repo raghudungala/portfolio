64c6bb0ed377404c8a51903fae810a9dbf3b567d
deepthichandrikav <deepthichandrika59@gmaiul.com>
2023-09-07 14:35:48 -0500

    Added footer

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
           <h2 className='prof'> Software Engineer</h2>
         </div>
         <div>
-          <p className="about-intro">I'm a software engineer, having a strong background in programming languages such as C, Python, C#, JavaScript, TypeScript, and R. Also have expertise in web development technologies like HTML, CSS, React.js, Redux, Flask, and ASP.NET. Additionally, I'm familiar with databases like MySQL and SQL and have experience with version control tools like Git and continuous integration/continuous deployment (CI/CD) pipelines using Azure Jenkins.</p>
+          <p className="about-intro">I'm a skilled software developer knowledgeable in JavaScript, Python, and a passion for frontend development and UI design. I'm a quick learner and enjoy collaborating closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. I have expertise in React.js framework and I am committed to staying up-to-date with the latest technologies and best practices in the field. I am a bilingual individual fluent in both English and Vietnamese. I have a background in the medical field, but have transitioned into tech industry to express my creativity through programming. I have a passion for coding and climbing, and enjoy pursuing activities that challenge both mentally and physically. With my diverse skill set and interests, I can contribute to a wide range of projects and teams.</p>
         </div>
       </div>
     </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
+import "../styles/footer.css"
 
 const Footer = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handelSubmit=(e)=>{
+    console.log(name)
+    console.log(email)
+    console.log(message)
+  }
   return (
     <footer>
+      <p>Get In Touch</p>
+      <div className='card'> 
+        <div className='contact_form'>
+          <label>Your Name</label><br></br>
+          <input type='text' placeholder='Your Name' value={name} onChange={e=>{setName(e.target.value)}}></input><br></br>
+          <label>Your Email</label><br></br>
+          <input type='email' placeholder='Email' value={email} onChange={e=>{setEmail(e.target.value)}}></input><br></br>
+          <label>Your Message</label><br></br>
+          <textarea type='email' placeholder='Write Message' value={message} onChange={e=>{setMessage(e.target.value)}}></textarea><br></br>
+          <button type='button' onClick={handelSubmit}>Submit</button>
+        </div>
+      </div>
+
       <p>&copy; {new Date().getFullYear()} Raghu</p>
     </footer>
   );
