b8f46c3537b4a825c67dd8e65e5008c0dbcfe44f
Raghu <draghu222@gmail.com>
2023-10-28 20:08:32 -0500

    Modified the welcome page styles

--- src/components/welcomepage.js
+++ src/components/welcomepage.js
@@ -1,22 +1,40 @@
 import React from "react";
 import "../styles/welcomepage.css";
 import personalImage from "../images/raghu.JPEG";
-// import award from "../images/award.png"
-// import star from "../images/star.png"
 
 const WelcomePage = () => {
+  const addSpaces = (text) => {
+    return text.split("").map((char, index) =>
+      char !== " " ? (
+        <span style={{ "--i": index }} key={index}>
+          {char}
+        </span>
+      ) : (
+        <>&nbsp;</>
+      )
+    );
+  };
+
+  const nameWithSpaces = addSpaces("Raghu");
+
   return (
     <section id="welcomepage">
       <div className="circular-image">
-        <img src={personalImage} alt="Personal" className="user-pic"></img>
+        <img src={personalImage} alt="Personal" className="user-pic" />
       </div>
       <h1 id="intro">
-        <span role="img" aria-label="hi">
-          👋
-        </span>
-        Hello I'm Raghu Dungala
+        {/* <span role="img" aria-label="hi"> */}
+        👋
+        {/* </span> */}
+        I'm &nbsp;
+        {nameWithSpaces}
+        <span style={{ "--i": 13 }}>.</span>
+        <span style={{ "--i": 14 }}>.</span>
+        <span style={{ "--i": 15 }}>.</span>
       </h1>
-      <h2>Coding with passion, building with excellence.</h2>
+      <h2 style={{ marginTop: "20px" }}>
+        Coding with passion, building with excellence.
+      </h2>
     </section>
   );
 };
